3804a9652aabb5c967d045268b37196967c1162e
qhuang05 <qhuang05@sina.com>
2020-08-19 10:18:33 +0800

    update

--- 03-server.js
+++ 03-server.js
@@ -3,16 +3,16 @@ const Vue = require('vue')
 const fs = require('fs')
 const path = require('path')
 const vueRenderer = require('vue-server-renderer').createRenderer({
-	template: fs.readFileSync(path.join(__dirname, '../public/template.html'), 'utf-8')
+	template: fs.readFileSync(path.join(__dirname, './public/template.html'), 'utf-8')
 })
 
 // 将创建vue实例分离到外部文件
-const createApp = require('../src/entry-server.js')
-server.get('*', async (req,res)=>{
+const createApp = require('./src/app.js')
+server.get('*', (req,res)=>{
 	res.status(200);
   	res.setHeader('Content-Type', 'text/html;charset=utf-8;')
 
-	let app = await createApp({url: req.url})
+	let {app} = createApp({url: req.url})
 	let info = {
 		title: '使用html页面模板做渲染',
 	}
@@ -23,4 +23,4 @@ server.get('*', async (req,res)=>{
 
 server.listen(9000, ()=>{
 	console.log('启动成功');
-}) 
+})
